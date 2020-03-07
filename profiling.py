f8b8ff599a7431ff3269d8970313e459e6ffc499
AdamKaco <kacoadam@gmail.com>
2020-03-07 18:07:56 +0100

    input, sum of inupt/nmb of items

--- profiling.py
+++ profiling.py
@@ -1,4 +1,16 @@
 import sys
+import re
+
+i = 0
+ans_x = 0
+nmbs = []
 
 for line in sys.stdin:
-    print(line)
+    nmbs = re.findall(r"\d+", line)
+
+for nmb in nmbs:
+    ans_x = ans_x + int (nmb) #call our function
+    i += 1
+
+ans_x = ans_x/i #call our function
+print (ans_x)
