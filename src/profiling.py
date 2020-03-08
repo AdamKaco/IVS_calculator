f306051e79e0b7474cddeae44118bd4dd5575d2b
AdamKaco <kacoadam@gmail.com>
2020-03-09 00:24:16 +0100

    experimenting with library and profiling to faster it

--- src/profiling.py
+++ src/profiling.py
@@ -1,17 +1,43 @@
-import sys
+"""
+findings function is 4% faster at 10 000 due to not using frozen librery
+at 100 they behave simularly
+at 10 as well
+
+"""
+
+
+import sys #later disabled
 import re
 import mathlib
-import sys
 
-file = open("input_data_prof_10000.txt")
-sys.stdin = file
+file = open("input_data_prof_10.txt") #later disabled
+sys.stdin = file #later disabled
 
 i = 0
+j = 0
+k = 0
 ans_x = 0
 nmbs = []
+nmbs.append(0)
 
 for line in sys.stdin:
     nmbs = re.findall(r"\d+", line)
+#slower by 68% and not working correctly
+"""
+for line in sys.stdin:
+    lengh = len(line)
+
+    for j in range (0, lengh):
+        if (line[j] == " "):
+            k = mathlib.sum(k, 1)
+            nmbs.append(0)
+        elif(line[j+1] == " " or j+1 == lengh):
+            nmbs[k] = mathlib.sum(nmbs[k], int(line[j]))
+        else:
+            nmbs[k] = mathlib.sum(nmbs[k], int(line[j]))
+            nmbs[k] = mathlib.mul(nmbs[k], 10)
+"""
+
 
 for nmb in nmbs:
     ans_x = mathlib.sum(ans_x, int(nmb))
