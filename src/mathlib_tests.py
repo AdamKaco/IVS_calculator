21f21b0c9d8fb74cae2a6f82cac746e95a27b71a
Simon Košina <simonkosina@gmail.com>
2020-03-09 11:32:49 +0100

    Added mathlib_tests.py

---
+++ src/mathlib_tests.py
@@ -0,0 +1,274 @@
+## @file mathlib_tests.py
+# @author Simon Kosina
+# @date 9.3.2020
+# @brief Tests for mathlib.py
+
+import unittest
+import mathlib
+
+## @brief Tests for addition
+class TestAddition(unittest.TestCase):
+
+    def test_sum(self):
+        # Test boundaries
+        self.assertEqual(mathlib.sum(1,0), 1)
+        self.assertEqual(mathlib.sum(-1,0), -1)
+        self.assertEqual(mathlib.sum(0,1), 1)
+        self.assertEqual(mathlib.sum(0,-1), -1)
+        
+        # Equals zero
+        self.assertEqual(mathlib.sum(0,0), 0)
+        self.assertEqual(mathlib.sum(-7,7), 0)
+        self.assertEqual(mathlib.sum(7,-7), 0)
+
+        # Pos and neg combinations for a > b
+        self.assertEqual(mathlib.sum(7,3),10)
+        self.assertEqual(mathlib.sum(7,-3),4)
+        self.assertEqual(mathlib.sum(-7,3),-4)
+        self.assertEqual(mathlib.sum(-7,-3),-10)
+        
+        # Pos and neg combinations for a < b
+        self.assertEqual(mathlib.sum(3,7),10)
+        self.assertEqual(mathlib.sum(-3,7),4)
+        self.assertEqual(mathlib.sum(3,-7),-4)
+        self.assertEqual(mathlib.sum(-3,-7),-10)
+
+## @brief Tests for subtraction
+class TestSubtraction(unittest.TestCase):
+
+    def test_sub(self):
+        # Test boundaries
+        self.assertEqual(mathlib.sub(1,0), 1)
+        self.assertEqual(mathlib.sub(-1,0), -1)
+        self.assertEqual(mathlib.sub(0,1), -1)
+        self.assertEqual(mathlib.sub(0,-1), 1)
+                                   
+        # Equals zero              
+        self.assertEqual(mathlib.sub(0,0), 0)
+        self.assertEqual(mathlib.sub(7,7), 0)
+        self.assertEqual(mathlib.sub(-7,-7), 0)
+                                   
+        # Pos and neg combinations for a > b
+        self.assertEqual(mathlib.sub(7,3),4)
+        self.assertEqual(mathlib.sub(7,-3),10)
+        self.assertEqual(mathlib.sub(-7,3),-10)
+        self.assertEqual(mathlib.sub(-7,-3),-4)
+                                   
+        # Pos and neg combinations for a < b
+        self.assertEqual(mathlib.sub(3,7),-4)
+        self.assertEqual(mathlib.sub(-3,7),-10)
+        self.assertEqual(mathlib.sub(3,-7),10)
+        self.assertEqual(mathlib.sub(-3,-7),4)
+        
+class TestMultiplication(unittest.TestCase):
+
+    def test_mul(self):
+        # Multiplication by 0
+        self.assertEqual(mathlib.mul(1,0), 0)
+        self.assertEqual(mathlib.mul(-1,0), 0)
+        self.assertEqual(mathlib.mul(0,1), 0)
+        self.assertEqual(mathlib.mul(0,-1), 0)
+        self.assertEqual(mathlib.mul(0,0), 0)
+        
+        # Pos and neg combinations for a = b
+        self.assertEqual(mathlib.mul(7,7), 49)
+        self.assertEqual(mathlib.mul(-7,7), -49)
+        self.assertEqual(mathlib.mul(7,-7), -49)
+        self.assertEqual(mathlib.mul(-7,-7), 49)
+                                   
+        # Pos and neg combinations for a > b
+        self.assertEqual(mathlib.mul(7,3),21)
+        self.assertEqual(mathlib.mul(7,-3),-21)
+        self.assertEqual(mathlib.mul(-7,3),-21)
+        self.assertEqual(mathlib.mul(-7,-3),21)
+                                   
+        # Pos and neg combinations for a < b
+        self.assertEqual(mathlib.mul(3,7),21)
+        self.assertEqual(mathlib.mul(-3,7),-21)
+        self.assertEqual(mathlib.mul(3,-7),-21)
+        self.assertEqual(mathlib.mul(-3,-7),21)
+
+class TestDivision(unittest.TestCase):
+    
+    def test_div(self):
+        # Division by 0
+        self.assertRaises(ZeroDivisionError, mathlib.div, 1, 0)
+        self.assertRaises(ZeroDivisionError, mathlib.div, -1, 0)
+
+        # Pos and neg combinations for a = b
+        self.assertEqual(mathlib.div(7,7), 1)
+        self.assertEqual(mathlib.div(-7,7), -1)
+        self.assertEqual(mathlib.div(7,-7), -1)
+        self.assertEqual(mathlib.div(-7,-7), 1)
+
+        # Pos and neg combinations for a > b
+        self.assertEqual(mathlib.div(10,2),5)
+        self.assertEqual(mathlib.div(10,-2),-5)
+        self.assertEqual(mathlib.div(-10,2),-5)
+        self.assertEqual(mathlib.div(-10,-2),5)
+                                   
+        # Pos and neg combinations for a < b
+        self.assertEqual(mathlib.div(2,10),0.2)
+        self.assertEqual(mathlib.div(-2,10),-0.2)
+        self.assertEqual(mathlib.div(2,-10),-0.2)
+        self.assertEqual(mathlib.div(-2,-10),0.2)
+
+        # Try some periodic results
+        self.assertAlmostEqual(mathlib.div(1,3), 1/3)
+
+class TestFactorial(unittest.TestCase):
+
+    def test_fact(self):
+        # n is not an integer
+        self.assertRaises(ValueError, mathlib.fact, 2.5)
+        self.assertRaises(ValueError, mathlib.fact, -3.121)
+        
+        # n < 0
+        self.assertRaises(ValueError, mathlib.fact, -1)
+        self.assertRaises(ValueError, mathlib.fact, -319)
+
+        # n = 0
+        self.assertEqual(mathlib.fact(0), 1)
+
+        # n > 0
+        self.assertEqual(mathllib.fact(1), 1)
+        self.assertEqual(mathllib.fact(3), 6)
+        self.assertEqual(mathllib.fact(10), 3628800)
+
+class TestExponentiation(unittest.TestCase):
+
+    def test_exp(self):
+        # n is not an integer
+        self.assertRaises(ValueError, mathlib.exp, 1, 2.5)
+
+        # x = 0
+        self.assertEqual(mathlib.exp(0,0),1)
+        self.assertEqual(mathlib.exp(0,1),0)
+        self.assertEqual(mathlib.exp(0,-1),0)
+        self.assertEqual(mathlib.exp(0,7),0)
+
+        # x < 0 and n = 0
+        self.assertEqual(mathlib.exp(-1,0),1)
+        self.assertEqual(mathlib.exp(-10,0),1)
+        
+        # x < 0 and n < 0
+        self.assertEqual(mathlib.exp(-1,-1),-1)
+        self.assertEqual(mathlib.exp(-1,-2),1)
+        self.assertEqual(mathlib.exp(-1,-5),-1)
+        self.assertEqual(mathlib.exp(-10,-1),-1/10)
+        self.assertEqual(mathlib.exp(-2.5,-2),1/(2.5*2.5))
+        self.assertEqual(mathlib.exp(-10,-5),-1/100000)
+
+        # x < 0 and n > 0
+        self.assertEqual(mathlib.exp(-1,1),-1)
+        self.assertEqual(mathlib.exp(-1,2),1)
+        self.assertEqual(mathlib.exp(-1,5),-1)
+        self.assertEqual(mathlib.exp(-10,1),-10)
+        self.assertEqual(mathlib.exp(-2.5,2),2.5*2.5)
+        self.assertEqual(mathlib.exp(-10,5),-100000)
+
+        # x > 0 and n = 0
+        self.assertEqual(mathlib.exp(1,0), 1)
+        self.assertEqual(mathlib.exp(10,0), 1)
+
+        # x > 0 and n < 0
+        self.assertEqual(mathlib.exp(1,-1),1)
+        self.assertEqual(mathlib.exp(1,-2),1)
+        self.assertEqual(mathlib.exp(1,-5),1)
+        self.assertEqual(mathlib.exp(10,-1),1/10)
+        self.assertEqual(mathlib.exp(2.5,-2),1/(2.5*2.5))
+        self.assertEqual(mathlib.exp(10,-5),1/100000)
+
+        # x > 0 and n > 0
+        self.assertEqual(mathlib.exp(1,1),1)
+        self.assertEqual(mathlib.exp(1,2),1)
+        self.assertEqual(mathlib.exp(1,5),1)
+        self.assertEqual(mathlib.exp(10,1),10)
+        self.assertEqual(mathlib.exp(2.5,2),2.5*2.5)
+        self.assertEqual(mathlib.exp(10,5),100000)
+
+class TestRoot(unittest.TestCase):
+
+    def test_root(self):
+        # n = 0
+        self.assertRaises(ValueError, mathlib.root, 2, 0)
+        
+        # n not an integer
+        self.assertRaises(ValueError, mathlib.root, 2, 1.5)
+        self.assertRaises(ValueError, mathlib.root, 2, -7.7)
+
+        # n > 0 and even
+        self.assertRaises(ValueError, mathlib.root, 2, -1) # x must be > 0        
+        # x > 0
+        self.assertEquals(mathlib.root(2, 4), 2)
+        self.assertEquals(mathlib.root(4, 16), 2)
+        # x = 0
+        self.assertEquals(mathlib.root(2, 0), 0)
+        self.assertEquals(mathlib.root(4, 0), 0)
+
+        # n > 0 and odd
+        # x < 0
+        self.assertEquals(mathlib.root(1, -3), -3)
+        self.assertEquals(mathlib.root(3, -27), -3)
+        # x > 0
+        self.assertEquals(mathlib.root(1, 3), 3)
+        self.assertEquals(mathlib.root(3, 27), 3)
+        # x = 0
+        self.assertEquals(mathlib.root(1, 0), 0)
+        self.assertEquals(mathlib.root(3, 0), 0)
+
+        # n < 0 and even
+        self.assertRaises(ValueError, mathlib.root, -2, -1) # x must be > 0        
+        # x > 0
+        self.assertEquals(mathlib.root(-2, 4), 0.5)
+        self.assertEquals(mathlib.root(-4, 16), 0.5)
+        # x = 0
+        self.assertRaises(ValueError, mathlib.root, -2, 0)
+        
+        # n < 0 and odd
+        # x < 0
+        self.assertEquals(mathlib.root(-1, -2), -0.5)
+        self.assertEquals(mathlib.root(-5, -3125), -0.2)
+        # x > 0
+        self.assertEquals(mathlib.root(-1, 2), 0.5)
+        self.assertEquals(mathlib.root(-5, 3125), 0.2)
+        # x = 0
+        self.assertRaises(ValueError, mathlib.root, -5, 0)
+
+class TestCombination(unittest.TestCase):
+
+    def test_comb(self):
+        # n or k is not an integer
+        self.assertRaises(ValueError, mathlib.comb, 2.5, 1)
+        self.assertRaises(ValueError, mathlib.comb, 5, 2.5)
+        self.assertRaises(ValueError, mathlib.comb, 7.7, 5.5)
+
+        # n or k is not non-negative
+        self.assertRaises(ValueError, mathlib.comb, 2, -1)
+        self.assertRaises(ValueError, mathlib.comb, -2, 1)
+        self.assertRaises(ValueError, mathlib.comb, -1, -5)
+
+        # n > k
+        self.assertRaises(ValueError, mathlib.comb, 1, 2)
+        self.assertRaises(ValueError, mathlib.comb, 0, 1)
+
+        # k = 0
+        self.assertEquals(mathlib.comb(2,0), 1)
+        self.assertEquals(mathlib.comb(5,0), 1)
+                                             
+        # k = 1                              
+        self.assertEquals(mathlib.comb(2,1), 2)
+        self.assertEquals(mathlib.comb(5,1), 5)
+                                             
+        # k = n                              
+        self.assertEquals(mathlib.comb(2,2), 1)
+        self.assertEquals(mathlib.comb(5,5), 1)
+
+        # some other values
+        self.assertEquals(mathlib.comb(7,3), 35)
+        self.assertEquals(mathlib.comb(13,2), 78)
+        self.assertEquals(mathlib.comb(25,3), 2300)
+
+
+if __name__ == "__main__":
+    unittest.main()
